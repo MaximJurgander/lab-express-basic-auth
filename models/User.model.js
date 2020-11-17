f5f8f4e9b9db42ff7f94d9c37df818584668e963
Maxim Jurgandere <maxim.jurgander@gmail.com>
2020-11-17 22:50:47 +0100

    first iteration done

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,17 @@
 // User model here
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Need username']
+  },
+  password: {
+    type: String,
+    required: [true, 'Need password']
+  }
+});
+
+
+
+module.exports = mongoose.model('User', userSchema);
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User.model');
+const bcrypt = require('bcrypt');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/register', (req, res, next) => {
+  res.render('register');
+});
+
+router.post('/register', async (req, res, next) => {
+  const { username, password } = req.body;
+  const hash = await bcrypt.hash(password, 10);
+  const user = new User({
+    username: username,
+    password: hash
+  });
+  await user.save();
+  res.redirect('/');
+});
 
 module.exports = router;
